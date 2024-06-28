d8abd0f26977170e90a16aa54705812ca5b22e53
Jabulani00 <jabulanimosesgwala@gmail.com>
2024-06-28 10:59:10 +0200

    fire

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -16,8 +16,8 @@ export class LoginPage implements OnInit {
   password: string = '';
 
   // Default admin credentials
-  defaultAdminEmail: string = 'admin@best.com';
-  defaultAdminPassword: string = '@bestB1234';
+  defaultAdminEmail: string = 'medi@pharm.com';
+  defaultAdminPassword: string = '@MediP1234';
 
   constructor(
     private router: Router,
